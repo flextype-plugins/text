1655a4f246da62d6c75eb63431a439f75e59782c
Awilum <awilum@msn.com>
2019-01-17 02:42:03 +0300

    Text 1.0.2

--- text.php
+++ text.php
@@ -20,35 +20,35 @@ use Flextype\Component\{Event\Event, Text\Text};
 Event::addListener('onShortcodesInitialized', function () {
 
     // Shortcode: [text-cut length=10]Lorem ipsum dolor sit amet, consectetur adipisicing elit[/text-cut]
-    Content::shortcode()->addHandler('text-cut', function(ShortcodeInterface $s) {
+    Entries::shortcode()->addHandler('text-cut', function(ShortcodeInterface $s) {
         return Text::cut($s->getContent(),
                          $s->getParameter('length'),
                          ((null !== $s->getParameter('msg')) ? $s->getParameter('msg') : '...' ));
     });
 
     // Shortcode: [text-lowercase]Text here[/text-lowercase]
-    Content::shortcode()->addHandler('text-lowercase', function(ShortcodeInterface $s) {
+    Entries::shortcode()->addHandler('text-lowercase', function(ShortcodeInterface $s) {
         return Text::lowercase($s->getContent());
     });
 
     // Shortcode: [text-uppercase]Text here[/text-uppercase]
-    Content::shortcode()->addHandler('text-uppercase', function(ShortcodeInterface $s) {
+    Entries::shortcode()->addHandler('text-uppercase', function(ShortcodeInterface $s) {
         return Text::uppercase($s->getContent());
     });
 
     // Shortcode: [text-lorem num=3]
-    Content::shortcode()->addHandler('text-lorem', function(ShortcodeInterface $s) {
+    Entries::shortcode()->addHandler('text-lorem', function(ShortcodeInterface $s) {
         return Text::lorem(((null !== $s->getParameter('num')) ? $s->getParameter('num') : 1 ));
     });
 
     // Shortcode: [text-right num=3]Text here[/text-right]
-    Content::shortcode()->addHandler('text-right', function(ShortcodeInterface $s) {
+    Entries::shortcode()->addHandler('text-right', function(ShortcodeInterface $s) {
         return Text::right($s->getContent(),
                            $s->getParameter('num'));
     });
 
     // Shortcode: [text-left num=3]Text here[/text-left]
-    Content::shortcode()->addHandler('text-left', function(ShortcodeInterface $s) {
+    Entries::shortcode()->addHandler('text-left', function(ShortcodeInterface $s) {
         return Text::left($s->getContent(),
                            $s->getParameter('num'));
     });
